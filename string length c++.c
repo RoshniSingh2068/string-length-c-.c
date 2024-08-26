ff45e0aa5546a386abee02db03fa0011c51ad056
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:55:37 +0530

    Create string length c++.c

---
+++ string length c++.c
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main (){
+    string txt = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+    cout << "the length of thw string is:" << txt.length();
+    return 0;
+
+}
